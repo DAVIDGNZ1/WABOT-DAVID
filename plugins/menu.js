a835532184958523d2aad7a932879f78168f4fb9
DAVIDGNZ1 <81473463+DAVIDGNZ1@users.noreply.github.com>
2021-04-11 12:31:02 +0700

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -8,6 +8,7 @@ let handler  = async (m, { conn, usedPrefix: _p }) => {
     let { min, xp, max } = levelling.xpRange(level, global.multiplier)
     let name = conn.getName(m.sender)
     let d = new Date
+    let pp = './img/menu.jpg'
     let locale = 'id'
     let gmt = new Date(0).getTime() - new Date('1 January 1970').getTime()
     let weton = ['Pahing', 'Pon','Wage','Kliwon','Legi'][Math.floor(((d * 1) + gmt) / 84600000) % 5]
@@ -125,6 +126,7 @@ let handler  = async (m, { conn, usedPrefix: _p }) => {
     }
     text = text.replace(new RegExp(`%(${Object.keys(replace).join`|`})`, 'g'), (_, name) => ''+replace[name])
     conn.reply(m.chat, text.trim(), m)
+    conn.sendFile (m.chat, pp, 'pp.jpg', text.trim())
   } catch (e) {
     conn.reply(m.chat, 'Maaf, menu sedang error', m)
     throw e
